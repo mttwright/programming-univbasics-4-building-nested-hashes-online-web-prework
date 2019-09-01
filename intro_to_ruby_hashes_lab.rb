c245461cbfedc7b06ecb30de70e255b27a414ecc
virtual-frame-7789 <mttwright@gmail.com>
2019-09-01 22:50:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,7 +24,7 @@ def monopoly_with_third_tier
     :railroads => {
       :pieces => 4, 
       :rent_in_dollars => {
-        :ham => "", :milk => '', :ciabatta => "", :steak => ''
+        :names => "", :milk => '', :ciabatta => "", :steak => ''
       } 
     }
   }
